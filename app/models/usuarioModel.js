7e30173171ea15ff0bbcfd0c7668d6e2ccd02692
giovaniwingter <giovaniwingter@hotmail>
2025-08-18 21:15:09 -0300

    atualilzação 18/08

--- app/models/usuarioModel.js
+++ app/models/usuarioModel.js
@@ -1,22 +1,21 @@
 const pool = require("../../config/pool_conexoes");
 
 const usuarioModel = {
-
     //CRUD -> Create retrieve update delete
-
     findAll: async()=>{
         try{
-            const [resultado, estrutura] = await pool.query("select * from usuario where status_usuario = 1");
+            const [resultado, estrutura] = await 
+            pool.query("select * from usuario where status_usuario = 1");
             return resultado;
         }catch(erro){
             console.log(erro);     
             return null;       
         }
     },
-
     create: async(valoresCampos)=>{
         try{
-            const [resultado] = await pool.query("insert into usuario set ?", [valoresCampos]);
+            const [resultado] = await 
+            pool.query("insert into usuario set ?", [valoresCampos]);
             return resultado;
         }catch(erro){
             console.log(erro);
@@ -25,7 +24,9 @@ const usuarioModel = {
     },    
     update: async(valoresCampos, id)=>{
         try{
-            const [resultado] = await pool.query("update usuario set ? where id_usuario = ?", [valoresCampos, id]);
+            const [resultado] = await 
+            pool.query("update usuario set ? where id_usuario = ?", 
+                [valoresCampos, id]);
             return resultado;
         }catch(erro){
             console.log(erro);
@@ -34,7 +35,8 @@ const usuarioModel = {
     },
     delete: async (id)=>{ // delete físico -> exclui do banco de dados
         try{
-           const [resultado] = await pool.query("delete from usuario where id_usuario = ?", [id]);
+           const [resultado] = await 
+           pool.query("delete from usuario where id_usuario = ?", [id]);
            return resultado;     
         }catch(erro){
             console.log(erro);
@@ -43,7 +45,9 @@ const usuarioModel = {
     },
     deleteLogico: async (id)=>{ // delete lógico -> torna o registro inativo
         try{
-            const [resultado] = await pool.query("upadate usuario set status_usuario = 0  where id_usuario = ?", [id]);
+            const [resultado] = await 
+            pool.query("update usuario set status_usuario = 0  where id_usuario = ?",
+                 [id]);
             return resultado;
         }catch(erro){
             console.log(erro);
@@ -51,7 +55,6 @@ const usuarioModel = {
         }
     }
 };
-
 
 module.exports = {usuarioModel}
 
