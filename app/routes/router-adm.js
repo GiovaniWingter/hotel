e9e87f3b36b002104252b13f8b344a7859ce10e1
prof Giovani <giovani.wingter@docente.fieb.edu.br>
2025-08-20 10:12:43 -0300

    atualização 19-08

--- app/routes/router-adm.js
+++ app/routes/router-adm.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const {usuarioModel} = require("../models/usuarioModel");
+
 
 router.get("/", (req, res)=>{
     res.render("pages/index-adm");
 })
 
-router.get("/adm-cliente", (req, res)=>{
-    res.render("pages/adm-cliente")
+router.get("/adm-cliente", async (req, res)=>{
+
+    const [listaClientes] = await usuarioModel.findAll(); 
+
+    res.render("pages/adm-cliente", {lista:listaClientes});
 })
 
 
