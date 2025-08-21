aa64ea9f7c658f79a23f803acdc760abccc2e594
giovaniwingter <giovaniwingter@hotmail>
2025-08-20 22:05:00 -0300

    ajuste 20-08

--- app/routes/router-adm.js
+++ app/routes/router-adm.js
@@ -3,19 +3,16 @@ const router = express.Router();
 
 const {usuarioModel} = require("../models/usuarioModel");
 
-const {usuarioModel} = require("../models/usuarioModel");
-
-
 router.get("/", (req, res)=>{
     res.render("pages/index-adm");
-})
+});
 
 router.get("/adm-cliente", async (req, res)=>{
 
     const listaClientes = await usuarioModel.findAll(); 
 
     res.render("pages/adm-cliente", {lista:listaClientes});
-})
+});
 
 
 module.exports = router;
